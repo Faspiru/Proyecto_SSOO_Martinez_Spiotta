8e1f8ba6f0be874134b10b5ec4055c4741e9dbed
faspiru <fabrizio.spiotta@correo.unimet.edu.ve>
2024-01-21 19:21:17 -0400

    Adding Main Class :sparkles:

---
+++ Proyecto1_SSOO_Martinez_Spiotta/src/proyecto1_ssoo_martinez_spiotta/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto1_ssoo_martinez_spiotta;
+
+/**
+ *
+ * @author fabriziospiotta
+ */
+public class Main {
+    
+}
