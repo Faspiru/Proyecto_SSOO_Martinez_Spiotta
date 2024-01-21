86442f5344c8c2a813bf50ab82b9d8ea8923b120
faspiru <fabrizio.spiotta@correo.unimet.edu.ve>
2024-01-21 19:19:27 -0400

    Creating Project :sparkles:

---
+++ Proyecto1_SSOO_Martinez_Spiotta/src/proyecto1_ssoo_martinez_spiotta/Proyecto1_SSOO_Martinez_Spiotta.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto1_ssoo_martinez_spiotta;
+
+/**
+ *
+ * @author fabriziospiotta
+ */
+public class Proyecto1_SSOO_Martinez_Spiotta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
